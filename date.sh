718a612144d7b8d5156b6ba465f828acd30da714
Steven <steven001@gmail.com>
2020-08-16 14:20:52 +1000

    initial commit

---
+++ date.sh
@@ -0,0 +1,23 @@
+t=0
+
+locations=(
+  Australia/Melbourne
+  Europe/Amsterdam
+  America/New_York
+)
+
+toggle() {
+  case $1 in
+    up) t=$(((t - 1) % ${#locations[@]})) ;;
+  down) t=$(((t + 1) % ${#locations[@]})) ;;
+  esac
+}
+
+trap "toggle up" USR1
+trap "toggle down" USR2
+
+while true; do
+  TZ=${locations[$t]} date +" %H:%M %Z %A %d %B%Y"
+  sleep 1 &
+  wait
+done
